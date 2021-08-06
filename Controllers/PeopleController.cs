f2ceda0b126b4e174a6408d1bdc1df0ef88ec268
Federico Silva Armas <farmas@uw.edu>
2021-08-06 10:08:14 -0700

    Sample project.

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using UwApi.Models;
+
+namespace UwApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        private static readonly string[] Names = new[]
+        {
+            "Federico", "Andrew", "Huiai", "Jennifer", "Jay"
+        };
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Person
+            {
+                RegId = rng.Next(-20, 55).ToString(),
+                Name = Names[rng.Next(Names.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,8 @@
+namespace UwApi.Models
+{
+    public class Person
+    {
+        public string RegId { get; set; }
+        public string Name { get; set; }
+    }
+}
